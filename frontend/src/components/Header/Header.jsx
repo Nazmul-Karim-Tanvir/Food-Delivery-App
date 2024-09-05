9372d1c01f029a2b65a71ffb8f9bca1591d1edc1
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2024-09-05 16:23:54 +0600

    header done

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header-contents">
+                <h2>
+                    Order your fabourite food here
+                </h2>
+                <p>
+                    Enjoy our wide variety of dishes and delicious meals.
+                    Order online and enjoy a taste of our culinary excellence. Delicious food is also available in various price ranges for various occations.
+                </p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header

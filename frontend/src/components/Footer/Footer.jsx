b120413ad380382472cebe58e4eadc8a668e1fe1
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2024-09-08 23:56:12 +0600

    footer complete

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+    return (
+        <div className='footer' id='footer'>
+            <div className="footer-content">
+                <div className="footer-content-left">
+                    <img src={assets.logo} alt="" />
+                    <p>xperience the finest flavors with our signature touch. Yummy, delicious, and crafted to perfection. Visit us for an unforgettable culinary journey! </p>
+                    <div className="footer-social-icons">
+                        <img src={assets.facebook_icon} alt="" />
+                        <img src={assets.twitter_icon} alt="" />
+                        <img src={assets.linkedin_icon} alt="" />
+                    </div>
+                </div>
+                <div className="footer-content-center">
+                    <h2>COMPANY</h2>
+                    <ul>
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li>Delivery</li>
+                        <li>Privecy policy</li>
+                    </ul>
+                </div>
+                <div className="footer-content-right">
+                    <h2>GET IN TOUCH</h2>
+                    <ul>
+                        <li>+88 01867737673</li>
+                        <li>contact@tomato.com</li>
+                    </ul>
+                </div>
+            </div>
+            <hr />
+            <p className="footer-copyright">Copyright 2024 @ Tomato.com - All Right Reserved</p>
+        </div>
+    )
+}
+
+export default Footer

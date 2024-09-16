005debe88e31c6c9ee43a2b547bb54cc9d82063c
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2024-09-16 12:19:15 +0600

    CSS start for login popup

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -17,8 +17,13 @@ const LoginPopup = ({ setShowLogin }) => {
                 </div>
                 <button>{currState === "Sign Up" ? "Create account" : "Login"}</button>
                 <div className="login-popup-condition">
-                    
+                    <input type="checkbox" required />
+                    <p>By continuing, i agree to the terms of use & privacy policy.</p>
                 </div>
+                {currState === "Login" ?
+                    <p>Create a new account? <span onClick={() => setCurrState("Sign Up")}>Click here</span></p>
+                    : <p>Already have an account? <span onClick={() => setCurrState("Login")}>Login here</span></p>
+                }
             </form>
         </div>
     )

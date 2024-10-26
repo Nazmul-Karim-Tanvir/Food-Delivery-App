faac03946c0989652a8df9ca9ff0247eae965c7e
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2024-10-26 22:00:22 +0600

    working on user controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,16 @@
+import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import validator from "validator";
+
+// login User
+const loginUser = async (user) => {
+
+}
+
+// register User
+const registerUser = async (user) => {
+
+}
+
+export { loginUser, registerUser }

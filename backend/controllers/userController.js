43c84413b73507c86f92af512de96327036fbb1d
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2024-10-27 17:21:43 +0600

    login api working

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,11 +5,28 @@ import validator from "validator";
 
 // login User
 const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await userModel.findOne({ email })
+        if (!user) {
+            return res.json({ success: false, message: "User not found" })
+        }
 
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.json({ success: false, message: "Invalid credentials" })
+        }
+        const token = await createToken(user._id);
+        res.json({ success: true, token })
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Error" })
+
+    }
 }
 
 const createToken = async (id) => {
-    return jwt.sign({ id },process.env.JWT_SECRET)
+    return jwt.sign({ id }, process.env.JWT_SECRET)
 }
 
 // register User
@@ -27,7 +44,7 @@ const registerUser = async (req, res) => {
             return res.json({ success: false, message: "Please enter valid email" });
         }
 
-        if (password.lenth < 8) {
+        if (password.length < 8) {
             return res.json({ success: false, message: "Please enter a strong password 8 characters at least " })
         }
 
@@ -44,13 +61,13 @@ const registerUser = async (req, res) => {
 
         // saving user to database
         const user = await newUser.save();
-        const token = createToken(user._id)
+        const token = await createToken(user._id)
         res.json({ success: true, token });
 
     } catch (error) {
         console.log(error);
         res.json({ success: false, message: "Error" });
-        
+
     }
 }
 

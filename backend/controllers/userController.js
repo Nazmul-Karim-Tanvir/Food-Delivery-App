21d3df1309ce2319a8f33ce5836d4bb5857032e5
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2024-10-28 14:59:25 +0600

    api working for login register

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,19 +9,20 @@ const loginUser = async (req, res) => {
     try {
         const user = await userModel.findOne({ email })
         if (!user) {
-            return res.json({ success: false, message: "User not found" })
+            return res.json({ success: false, message: "User Doesn't exist" })
         }
 
         const isMatch = await bcrypt.compare(password, user.password);
+
         if (!isMatch) {
             return res.json({ success: false, message: "Invalid credentials" })
         }
         const token = await createToken(user._id);
         res.json({ success: true, token })
+
     } catch (error) {
         console.log(error);
         res.json({ success: false, message: "Error" })
-
     }
 }
 

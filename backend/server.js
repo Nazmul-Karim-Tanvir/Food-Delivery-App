0afb1a1c673c4ffb958ee5d34d15cc5449947f22
NAZMUL KARIM TANVIR <135350264+Nazmul-Karim-Tanvir@users.noreply.github.com>
2024-10-29 13:04:54 +0600

    upto 7.10 done

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
 import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js";
 // app config
 
 const app = express();
@@ -19,14 +20,15 @@ connectDB();
 
 //api endpoints
 app.use("/api/food", foodRouter)
-app.use("/images",express.static("uploads"))
-app.use("/api/user",userRouter)
+app.use("/images", express.static("uploads"))
+app.use("/api/user", userRouter)
+app.use("/api/cart", cartRouter);
 
 app.get("/", (req, res) => {
     res.send("API Working");
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server Started on  http://localhost:${port}`);
 })
 
